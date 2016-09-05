f7e63f04dc7463484e9e4ea44a6f58f69da6041a
ChienHsin-Chan <hcchien@gmail.com>
2016-09-06 01:00:15 +0800

    update the schema

--- models/PostCategory.js
+++ models/PostCategory.js
@@ -9,8 +9,7 @@ var PostCategory = new keystone.List('PostCategory', {
 
 PostCategory.add({
 	name: { label: "名稱", type: String, required: true },
-    type: { label: "類型", type: Types.Select, options: 'articles, file, link', default: 'articles' },
-    file: { label: "檔案", type: Types.ImageRelationship, ref: 'Document', dependsOn: { type: "file" } },
+    type: { label: "類型", type: Types.Select, options: 'articles, link', default: 'articles' },
     link: { label: "連結", type: String, dependsOn: { type: "link" } },
 
 });
