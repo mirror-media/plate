0f08ac2f8f3f8055de15342115d1d561d259d28d
ChienHsin-Chan <hcchien@gmail.com>
2016-09-08 00:42:16 +0800

    add the keystne editor for the editorial choices

---
+++ models/Choice.js
@@ -0,0 +1,18 @@
+var keystone = require('arch-keystone');
+var transform = require('model-transform');
+var Types = keystone.Field.Types;
+
+var Choice = new keystone.List('Choice', {
+	autokey: { from: 'pickDate', path: 'key', unique: true },
+    defaultSort: '-pickDate',
+});
+
+Choice.add({
+	pickDate: { label: '日期', type: Types.Datetime, default: Date.now, required: true },
+    choices: { label: '精選文章', type: Types.Relationship, ref: 'Post', many: true },
+});
+
+Choice.relationship({ ref: 'Post', refPath: 'choices' });
+
+transform.toJSON(Choice);
+Choice.register();
