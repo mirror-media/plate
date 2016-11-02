d6f528befab2a66b7e93b43bfbe81bed169aa7df
ChienHsin-Chan <hcchien@gmail.com>
2016-11-02 16:27:47 +0800

    fields updated for the topics and postcategories

--- models/Topic.js
+++ models/Topic.js
@@ -6,7 +6,17 @@ var Topic = new keystone.List('Topic', {
 });
 
 Topic.add({
-	name: { label: '專題名稱', type: String, required: true },
+  name: { label: '專題名稱', type: String, required: true },
+  brief: { label: '前言', type: Types.Html, wysiwyg: true, height: 150 },
+  heroVideo: { label: 'Leading Video', type: Types.Relationship, ref: 'Video' },
+  heroImage: { label: '首圖', type: Types.ImageRelationship, ref: 'Image' },
+  heroImageSize: { label: '首圖尺寸', type: Types.Select, options: 'extend, normal, small', default: 'normal', dependsOn: { heroImage: {'$regex': '.+/i'}}},
+  og_title: { label: 'FB分享標題', type: String, require: false},
+  og_description: { label: 'FB分享說明', type: String, require: false},
+  og_image: { label: 'FB分享縮圖', type: Types.ImageRelationship, ref: 'Image' },
+  isFeatured: { label: '置頂', type: Boolean, index: true },
+  style: { label: 'CSS', type: Types.Textarea },
+  javascript: { label: 'javascript', type: Types.Textarea },
 });
 
 Topic.relationship({ ref: 'Post', refPath: 'topics' });
