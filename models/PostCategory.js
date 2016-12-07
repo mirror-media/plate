7b66814aa9cb41bdc2c8c61a51e6a2d629b1c5b6
ChienHsin-Chan <hcchien@gmail.com>
2016-12-07 14:19:43 +0800

    add style and script fields into the PostCategory

--- models/PostCategory.js
+++ models/PostCategory.js
@@ -12,6 +12,12 @@ PostCategory.add({
   name: { label: "名稱", type: String, required: true },
   title: { label: "中文名稱", type: String, required: true, default: "分類" },
   isFeatured: { label: '置頂', type: Boolean, index: true },
+  style: { type: Types.Select, options: 'feature, listing, tile', default: 'feature' },
+  og_title: { label: 'FB分享標題', type: String, require: false},
+  og_description: { label: 'FB分享說明', type: String, require: false},
+  og_image: { label: 'FB分享縮圖', type: Types.ImageRelationship, ref: 'Image' },
+  css: { label: 'CSS', type: Types.Textarea },
+  javascript: { label: 'javascript', type: Types.Textarea },
 });
 
 PostCategory.relationship({ ref: 'Post', refPath: 'categories' });
