c37fda05d12e4fabc68689cbef65cf8daa12e878
ChienHsin-Chan <hcchien@gmail.com>
2016-12-19 11:50:37 +0800

    add a new style for the Section

--- models/Section.js
+++ models/Section.js
@@ -16,7 +16,7 @@ Section.add({
   categories: { label: '分類', type: Types.Relationship, ref: 'PostCategory', many: true },
   extend_cats: { label: '其他分類', type: Types.Relationship, ref: 'PostCategory', many: true },
   isFeatured: { label: '置頂', type: Boolean, index: true },
-  style: { type: Types.Select, options: 'feature, listing, tile', default: 'feature' },
+  style: { type: Types.Select, options: 'feature, listing, tile, full', default: 'feature' },
   og_title: { label: 'FB分享標題', type: String, require: false },
   og_description: { label: 'FB分享說明', type: String, require: false },
   og_image: { label: 'FB分享縮圖', type: Types.ImageRelationship, ref: 'Image' },
