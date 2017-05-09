b917853c04da4082614cf7c6995247720a807ae7
ChienHsin-Chan <hcchien@gmail.com>
2017-05-09 15:21:03 +0800

    add an event type called "logo" to modify the logo for the events

--- models/Event.js
+++ models/Event.js
@@ -12,7 +12,7 @@ Event.add({
   name: { type: String, initial: true, required: true, index: true },
   state: { label: '狀態', type: Types.Select, options: 'draft, published', default: 'draft', index: true },
   sections: { label: '分區', type: Types.Relationship, ref: 'Section', many: true },
-  eventType: { type: Types.Select, options: [ 'embedded', 'video', 'image' ], index: true },
+  eventType: { type: Types.Select, options: [ 'embedded', 'video', 'image', 'logo' ], index: true },
   startDate: { type: Types.Datetime, initial: true, required: true },
   endDate: { type: Types.Datetime, initial: true },
   video: { label: 'Video', type: Types.Relationship, ref: 'Video', dependsOn: { 'eventType': 'video' } },
