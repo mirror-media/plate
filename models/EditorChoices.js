88b4c889c73f88f4748304cfc17b654eedf71f62
ChienHsin-Chan <hcchien@gmail.com>
2017-05-25 14:53:26 +0800

    new model for the editor choices

---
+++ models/EditorChoices.js
@@ -0,0 +1,18 @@
+var keystone = require('arch-keystone');
+var transform = require('model-transform');
+var Types = keystone.Field.Types;
+
+var EditorChoices = new keystone.List('EditorChoices', {
+    track: true,
+	sortable: true,
+});
+
+EditorChoices.add({
+    choices: { label: '精選文章', type: Types.Relationship, ref: 'Post', many: false },
+});
+
+EditorChoices.relationship({ ref: 'Post', refPath: 'choices' });
+EditorChoices.defaultColumns = 'choices';
+
+transform.toJSON(EditorChoices);
+EditorChoices.register();
