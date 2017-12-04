25acdb27462bd7c8da3830a9527e7eec8739d1f2
ChienHsin-Chan <hcchien@gmail.com>
2017-12-04 12:04:06 +0800

    add a new event type called "mod"

--- models/Event.js
+++ models/Event.js
@@ -12,11 +12,11 @@ Event.add({
   name: { type: String, initial: true, required: true, index: true },
   state: { label: '狀態', type: Types.Select, options: 'draft, published', default: 'draft', index: true },
   sections: { label: '分區', type: Types.Relationship, ref: 'Section', many: true },
-  eventType: { type: Types.Select, options: [ 'embedded', 'video', 'image', 'logo' ], index: true },
+  eventType: { type: Types.Select, options: [ 'embedded', 'video', 'image', 'logo', 'mod' ], index: true },
   startDate: { type: Types.Datetime, initial: true, required: true },
   endDate: { type: Types.Datetime, initial: true },
   video: { label: 'Video', type: Types.Relationship, ref: 'Video', dependsOn: { 'eventType': 'video' } },
-  embed: { label: 'Embedded code', type: String, dependsOn: { 'eventType': 'embedded' } },
+  embed: { label: 'Embedded code', type: String, dependsOn: { '$or': [ { 'eventType': 'embedded' }, { 'eventType': 'mod' } ] } },
   image: { label: 'Image', type: Types.ImageRelationship, ref: 'Image', dependsOn: { '$or': [ { 'eventType': 'image' }, { 'eventType': 'logo'} ] } },
   isFeatured: { label: '置頂', type: Boolean, index: true },
 });
