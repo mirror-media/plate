7e535a37ae688f5a10dd205334f1bcccf872efa7
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-03-02 11:54:35 +0800

    revise the progress of resizing & watermark adding (#20)

--- models/ImageCollection.js
+++ models/ImageCollection.js
@@ -27,27 +27,33 @@ ImageCollection.add({
             width: 2000,
             height: null,
             options: {
-              watermark: '/public/images/watermark-120x120.png'
+              watermark: '/public/images/20171025EA-0142.png',
+              watermarkBase: 2000
             }
         }, {
             target: 'tablet',
             width: 1200,
             height: null,
             options: {
-              watermark: '/public/images/watermark-120x120.png'
+              watermark: '/public/images/20171025EA-0142.png',
+              watermarkBase: 2000
             }
         }, {
             target: 'mobile',
             width: 800,
             height: null,
             options: {
-              watermark: '/public/images/watermark-120x120.png'
+              watermark: '/public/images/20171025EA-0142.png',
+              watermarkBase: 2000
             }
         }, {
             target: 'tiny',
             width: 150,
             height: null,
-            options: {}
+            options: {
+              watermark: '/public/images/20171025EA-0142.png',
+              watermarkBase: 2000
+            }
         }],
         extractIPTC: extractIPTC
     }
