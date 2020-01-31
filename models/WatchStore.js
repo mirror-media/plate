94216ea17678233d31a0a34d66eb06b314271b1b
Hsin-chan Chien <hcchien@gmail.com>
2020-01-31 13:41:00 +0800

    add default value in the store name

--- models/WatchStore.js
+++ models/WatchStore.js
@@ -12,7 +12,7 @@ var WatchStore = new keystone.List('WatchStore', {
 
 
 WatchStore.add({
-	name: { label: '店名', type: String, required: true, index: true },
+	name: { label: '店名', type: String, required: true, index: true, default: '請輸入店名' },
 	address: { label: '地址', type: String },
 	phone: { label: '電話', type: String },
 	map: { label: 'google map url', type: Types.Url },
