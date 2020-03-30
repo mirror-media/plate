637136f844c476e470712dce5b2299f88be3a662
Hsin-chan Chien <hcchien@gmail.com>
2020-03-30 21:33:26 +0800

    update the model of the editor choices

--- models/EditorChoices.js
+++ models/EditorChoices.js
@@ -9,13 +9,11 @@ var EditorChoices = new keystone.List('EditorChoices', {
 
 EditorChoices.add({
     choices: { label: '精選文章', type: Types.Relationship, ref: 'Post', many: false },
-    startDate: { type: Types.Datetime, initial: true, required: true },
-    endDate: { type: Types.Datetime, initial: true },
     state: { label: '狀態', type: Types.Select, options: 'draft, published, scheduled, archived, invisible', default: 'draft', index: true },
 });
 
 EditorChoices.relationship({ ref: 'Post', refPath: 'choices' });
-EditorChoices.defaultColumns = 'choices';
+EditorChoices.defaultColumns = 'choices, state';
 
 transform.toJSON(EditorChoices);
 EditorChoices.register();
