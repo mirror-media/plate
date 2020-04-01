ba61bafa0de465757e8031869c803ed2ccaab0da
Hsin-chan Chien <hcchien@gmail.com>
2020-04-01 17:59:58 +0800

    display the post title in relationship

--- models/Post.js
+++ models/Post.js
@@ -8,6 +8,7 @@ var Types = keystone.Field.Types;
 
 var Post = new keystone.List('Post', {
 	autokey: { path: 'slug', from: 'name', unique: true, fixed: true },
+	map: { name: 'title' },
     track: true,
     defaultSort: '-publishedDate',
 });
