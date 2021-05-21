a8d3c5e168b1668454587dee982a0f7087c300c5
Hsin-chan Chien <hcchien@gmail.com>
2021-05-21 19:30:40 +0800

    remove wide type from the Topic

--- models/Topic.js
+++ models/Topic.js
@@ -24,7 +24,7 @@ Topic.add({
   og_image: { label: 'FB分享縮圖', type: Types.ImageRelationship, ref: 'Image' },
   isFeatured: { label: '置頂', type: Boolean, index: true },
   title_style: { label: '專題樣式', type: Types.Select, options: 'feature', default: 'feature', index: true },
-  type: { label: '型態', type: Types.Select, options: 'list, timeline, group, portrait wall, wide', default: 'list', index: true },
+  type: { label: '型態', type: Types.Select, options: 'list, timeline, group, portrait wall', default: 'list', index: true },
   source: { label: '資料來源', type: Types.Select, options: 'posts, activities', dependsOn: { type: 'timeline' } },
   sort: { label: '時間軸排序', type: Types.Select, options: 'asc, desc', dependsOn: { type: 'timeline' } },
   style: { label: 'CSS', type: Types.Textarea },
